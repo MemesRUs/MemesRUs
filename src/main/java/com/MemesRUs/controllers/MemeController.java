c29917b0c7e75f7d2fdb2adf06ebd2852764d5a9
cpoakley1432 <cpoakley1432@gmail.com>
2015-11-19 14:17:51 -0500

    add login and logout

--- src/main/java/com/MemesRUs/controllers/MemeController.java
+++ src/main/java/com/MemesRUs/controllers/MemeController.java
@@ -1,9 +1,15 @@
 package com.MemesRUs.controllers;
 
+import com.MemesRUs.entities.User;
 import com.MemesRUs.services.MemeRepository;
 import com.MemesRUs.services.UserRepository;
+import com.MemesRUs.utils.PasswordHash;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 
 /**
  * Created by cameronoakley on 11/19/15.
@@ -15,4 +21,27 @@ public class MemeController {
 
     @Autowired
     UserRepository users;
+
+    @RequestMapping("/login")
+    public String login(String username, String password, HttpSession session) throws Exception {
+        session.setAttribute("username", username);
+
+        User user = users.findOneByName(username);
+        if (user == null){
+            user = new User();
+            user.username = username;
+            user.password = PasswordHash.createHash(password);
+            users.save(user);
+        }
+        else if (!PasswordHash.validatePassword(password, user.password)){
+            throw new Exception("Wrong password");
+        }
+        return "redirect:/";
+    }
+    @RequestMapping("/logout")
+    public String logout(HttpServletRequest request) {
+        HttpSession session = request.getSession();
+        session.invalidate();
+        return "redirect:/";
+    }
 }
--- src/main/java/com/MemesRUs/services/UserRepository.java
+++ src/main/java/com/MemesRUs/services/UserRepository.java
@@ -7,4 +7,5 @@ import org.springframework.data.repository.CrudRepository;
  * Created by cameronoakley on 11/19/15.
  */
 public interface UserRepository extends CrudRepository<User, Integer> {
+    User findOneByName(String name);
 }
