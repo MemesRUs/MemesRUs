8ab5c47890999e033a9fc852cf07f91356ac7170
earlbozarth <ehbbozarth@gmail.com>
2015-11-19 13:44:32 -0500

    Committ the rest

---
+++ src/main/java/com/MemesRUs/MemesRUsApplication.java
@@ -0,0 +1,12 @@
+package com.MemesRUs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MemesRUsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MemesRUsApplication.class, args);
+    }
+}
---
+++ src/test/java/com/MemesRUs/MemesRUsApplicationTests.java
@@ -0,0 +1,18 @@
+package com.MemesRUs;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = MemesRUsApplication.class)
+@WebAppConfiguration
+public class MemesRUsApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
