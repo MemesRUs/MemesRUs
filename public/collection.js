fb56f2d753e7797ab20a1fb1524a6da3fe0c4644
Juan Arbelaez <juanarbelaez@Juans-MacBook-Pro.local>
2015-11-19 14:59:58 -0500

    setup of files

---
+++ public/collection.js
@@ -0,0 +1,12 @@
+var Backbone = require('backbone');
+var $ = require('jquery');
+Backbone.$ = $;
+var _ = require('underscore');
+var tmpl = require('./templates');
+var MemeCollection = require('./collection');
+var HeaderView = require('./headerView');
+var FooterView = require('./footerView');
+var FormView = require('./formView');
+var MemeModel = require('./model');
+var MemeView = require('./modelView');
+var layoutView = require('./layoutView');
