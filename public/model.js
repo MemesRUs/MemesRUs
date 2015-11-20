242e8d411ecd3efbe8206d1998145db39da8eab7
Alison Arsenault <AlisonHorta@Alisons-MacBook-Pro.local>
2015-11-20 13:14:37 -0500

    meme models/collection/views setup with some styling

--- public/model.js
+++ public/model.js
@@ -4,9 +4,28 @@ Backbone.$ = $;
 var _ = require('underscore');
 var tmpl = require('./templates');
 var MemeCollection = require('./collection');
+var MemeCollectionView = require('./collectionView');
 var HeaderView = require('./headerView');
 var FooterView = require('./footerView');
 var FormView = require('./formView');
 var MemeModel = require('./model');
 var MemeView = require('./modelView');
 var layoutView = require('./layoutView');
+
+
+module.exports = Backbone.Model.extend({
+  urlRoot: 'http://tiny-tiny.herokuapp.com/collections/memeordeath',
+  idAttribute: '_id',
+  defaults: function() {
+    return {
+      topText: "generic top text",
+      botText: "generic bottom text",
+      image: "generic url",
+      author: "generic author",
+      likes: 0
+    };
+  },
+  initialize: function () {
+    console.log("meme models being created...");
+  }
+});
